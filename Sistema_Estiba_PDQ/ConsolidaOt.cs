303caec2ffb8e39e84293cf829c7d62ca5639c96
Fran1106 <fdaine@live.cl>
2024-07-04 16:59:04 -0400

    implementacion BD npgsql

---
+++ Sistema_Estiba_PDQ/ConsolidaOt.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+
+namespace Sistema_Estiba_PDQ;
+
+    private NpgsqlCommand com;
+    private NpgsqlDataReader read;
+
+    internal class ConsolidaOt
+    {
+    }
+
+    public void Consultar(string expedicion)
+    {
+
+        string query = "select * from  alerce.reporte_calidad where expedicion = " + expedicion;
+
+        NpgsqlCommand command = new NpgsqlCommand(query, conn);
+        NpgsqlDataReader reader = command.ExecuteReader();
+
+        Console.WriteLine("Select reader data: " + reader.HasRows);
+        Console.WriteLine("Consulta OK Rows obtenidos :  " + reader.Rows);
+
+        try
+        {
+            while (reader.Read())
+            {
+                int pvkilos = Convert.ToInt32(reader["PVKILOS"]);
+                int bultos = Convert.ToInt32(reader["BULTOS"]);
+                MessageBox.Show("Data expedicion: " + expedicion + " pvkilos: " + pvkilos + " bultos: " + bultos);
+            }
+        }
+        catch (NpgsqlException ex)
+        {
+            Console.WriteLine(ex.Message);
+            ProjectData.SetProjectError(ex);
+            MessageBox.Show("Error Al Generar El Código: " + ex.ToString());
+            ProjectData.ClearProjectError();
+        }
+    }
+
---
+++ Sistema_Estiba_PDQ/ModuleNpgSql.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft .VisualBasic.CompilerServices;
+using Npgsql;
+using System;
+using System.Windows.Forms;
+
+namespace Sistema_Estiba_PDQ;
+
+[StandardModule]
+internal sealed class ModuleNpgSql
+{
+    public static NpgsqlConnection connNpg;
+
+    public static void Conexion()
+    {
+        try
+        {
+            NpgsqlConnectionStringBuilder keyValuePairs = new NpgsqlConnectionStringBuilder();
+            keyValuePairs.Database = "pdq";
+            keyValuePairs.Host = "controlit.csu2b8boipfw.us-east-2.rds.amazonaws.com";
+            keyValuePairs.Username = "controlit";
+            keyValuePairs.Password = "Vo9xK5SSA5f5LNtJEhgT";
+            keyValuePairs.Port = 5432;
+            connNpg = new NpgsqlConnection(keyValuePairs.ConnectionString);
+            connNpg.Open();
+        }
+        catch (NpgsqlException ex) 
+        {
+            Console.WriteLine(ex.Message);
+            ProjectData.SetProjectError(ex);
+            MessageBox.Show("Error Al Generar El Código: " + ex.ToString());
+            ProjectData.ClearProjectError();
+        }
+    }
+
+    public static void Desconection()
+    { 
+        connNpg.Dispose();
+
+    }
+
+}
+
